9e0a5b928de34c067cdc66ef659d4f6d1de075ba
Ravikiran Chavan <varsha.khese@hindindustec.com>
2026-08-16 14:38:41 +0530

    resolve issue

--- salon-backend/src/main/java/website/salon/config/SecurityConfig.java
+++ salon-backend/src/main/java/website/salon/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package website.salon.config;
 
 import jakarta.servlet.http.HttpServletResponse;
 import tools.jackson.databind.ObjectMapper;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
